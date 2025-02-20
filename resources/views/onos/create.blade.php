887c9c25da612cd2bafd62c7baf6db30080826fb
Ken1213811 <24303587802qq@gmail.com>
2025-02-20 06:47:02 +0000

    tamesi

--- resources/views/onos/create.blade.php
+++ resources/views/onos/create.blade.php
@@ -32,7 +32,7 @@
             <input type="submit" value="store"/>
         </form>
         <div class="footer">
-            <a href="/">戻る</a>
+            <a href="/">back</a>
         </div>
     </body>
 </html>
